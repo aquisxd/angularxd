411415c96e71573f7108cc1a9e8c664b0ac4084e
aquisxd <jhan.capia@upeu.edu.pe>
2019-08-15 11:13:44 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'content',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ecom-ng';
+  username='Jhan Arnel';
+}
